6499163ff6c944f2c50b8dd6f2f94a7c8d1eb781
tomhuda <tomhuda@strobecorp.com>
2011-04-16 06:18:06 -0700

    Initial commit (Amber + Todos + HTML5 Boilerplate)

---
+++ js/todos.js
@@ -0,0 +1,86 @@
+// ==========================================================================
+// Project:   Todos
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+  title: null,
+  isDone: false
+});
+
+Todos.todoListController = SC.ArrayController.create({
+  content: [],
+
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  },
+
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this);
+  },
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
+});
+
+Todos.CreateTodoView = SC.TemplateView.extend(SC.TextFieldSupport, {
+  insertNewline: function() {
+    var value = this.get('value');
+
+    if (value) {
+      Todos.todoListController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+Todos.ClearCompletedView = SC.TemplateView.extend({
+  mouseUp: function() {
+    Todos.todoListController.clearCompletedTodos();
+  }
+});
+
+Todos.TodoListView = SC.TemplateCollectionView.extend({
+  contentBinding: 'Todos.todoListController'
+});
+
+Todos.CheckboxView = SC.TemplateView.extend(SC.CheckboxSupport, {
+  valueBinding: '.parentView.content.isDone'
+});
+
+Todos.StatsView = SC.TemplateView.extend({
+  remainingBinding: 'Todos.todoListController.remaining',
+
+  displayRemaining: function() {
+    var remaining = this.get('remaining');
+
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining').cacheable()
+});
+
+Todos.MarkAllDoneView = SC.TemplateView.extend(SC.CheckboxSupport, {
+  valueBinding: 'Todos.todoListController.allAreDone'
+});
+
+SC.ready(function() {
+  SC.TemplateView.create({
+    elementId: 'todos',
+    templateName: 'todos'
+  }).append();
+});
+
+
